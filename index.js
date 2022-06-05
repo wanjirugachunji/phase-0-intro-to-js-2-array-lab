4dd16f4312c1d6c765b876cdbdbbde56a7f35104
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-06 01:55:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+let cats =["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+cats.push(name)
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name){
+return cats.pop(name)
+function appendCat(name) {
+//  var shiro= [name, ...cats]
+//  return shiro 
+var c1=cats(cats.slice())
+c1.push(name)
+return c1}
+}
+
+function prependCat(name){
+  var cats2=[name, ...cats];
+  return cats2;
+}
+function removeLastCat(){
+  var cats2= cats.slice(0,cats.length-1);
+  return cats2;
+}
+function removeFirstCat(){
+  var cats2=cats.slice(1);
+  return cats2
+}
